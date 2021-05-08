cee2fc466d024f8ea45950f6423bccd0a1a22dd3
Najdi1996 <perel99@onet.pl>
2021-05-08 23:45:48 +0200

    1 zadanie cz.1

---
+++ 1 projekt/build/index.js
@@ -0,0 +1,94 @@
+var StatsApp = /** @class */ (function () {
+    //Konstruktor
+    function StatsApp() {
+        this.startApp();
+    }
+    StatsApp.prototype.startApp = function () {
+        this.getInputs();
+        this.watchInputValues();
+    };
+    //Query selectory do inputÃ³w
+    StatsApp.prototype.getInputs = function () {
+        this.Inputsnumber = document.querySelector('#inputs');
+        this.Inputs = document.querySelector('.input-data');
+        //this.dataInput1 =document.querySelector('#data1');
+        //this.dataInput2 =document.querySelector('#data2');
+        //this.dataInput3 =document.querySelector('#data3');
+        //this.dataInput4 =document.querySelector('#data4');
+        this.sumInput = document.querySelector('#sum');
+        this.avgInput = document.querySelector('#avg');
+        this.minInput = document.querySelector('#min');
+        this.maxInput = document.querySelector('#max');
+    };
+    //Tworzenie inputow
+    StatsApp.prototype.createInputs = function () {
+        var quantity = +this.Inputsnumber.value;
+        console.log(quantity);
+        this.clearContent(); //Jezeli cos instnialo wczesniej - usuwa to.
+        var _loop_1 = function (i) {
+            input = document.createElement("input");
+            input.type = "text";
+            input.className = "data" + i;
+            input.oninput = function () { };
+            button = document.createElement("button");
+            button.innerHTML = 'Delete';
+            button.className = "delete" + i;
+            button.onclick = function () {
+                var inputDiv = document.querySelector('.input-data');
+                var classname = document.querySelector('.delete' + i);
+                var dataname = document.querySelector('.data' + i);
+                var brname = document.querySelector('.br' + i);
+                inputDiv.removeChild(classname);
+                inputDiv.removeChild(dataname);
+                inputDiv.removeChild(brname);
+            };
+            br = document.createElement('br');
+            br.className = 'br' + i;
+            this_1.dataDiv = document.querySelector('.input-data').appendChild(input);
+            this_1.dataDiv = document.querySelector('.input-data').appendChild(button);
+            this_1.dataDiv = document.querySelector('.input-data').appendChild(br);
+        };
+        var this_1 = this, input, button, br;
+        for (var i = 1; i <= quantity; i++) {
+            _loop_1(i);
+        }
+    };
+    StatsApp.prototype.clearContent = function () {
+        this.dataDiv = document.querySelector('.input-data');
+        while (this.dataDiv.firstChild) {
+            this.dataDiv.removeChild(this.dataDiv.firstChild);
+        }
+    };
+    StatsApp.prototype.watchInputValues = function () {
+        var _this = this;
+        this.Inputsnumber.addEventListener('input', function () { return _this.createInputs(); });
+        //this.dataInput1.addEventListener('input', () => this.computeData());
+        //this.dataInput2.addEventListener('input', () => this.computeData());
+        //this.dataInput3.addEventListener('input', () => this.computeData());
+        //this.dataInput4.addEventListener('input', () => this.computeData());
+        this.Inputs.addEventListener('keypress', function () { return _this.computeData(); });
+        this.Inputs.addEventListener('click', function () { return _this.computeData(); });
+        //TO DO
+        //OGARNIJ ODSWIEZANIE
+    };
+    StatsApp.prototype.computeData = function () {
+        //const data1 = +this.dataInput1.value;
+        // const data2 = +this.dataInput2.value;
+        //const data3 = +this.dataInput3.value;
+        //const data4 = +this.dataInput4.value;
+        //const sum=data1+data2+data3+data4;
+        //const avg = sum/4;
+        //const min =Math.min(data1,data2,data3,data4);
+        //const max =Math.max(data1,data2,data3,data4);
+        //this.showStats(sum, avg, min, max);
+        console.log('dziala');
+    };
+    StatsApp.prototype.showStats = function (sum, avg, min, max) {
+        this.sumInput.value = sum.toString();
+        this.avgInput.value = avg.toString();
+        this.minInput.value = min.toString();
+        this.maxInput.value = max.toString();
+    };
+    return StatsApp;
+}());
+var App = new StatsApp();
--- 1 projekt/src/index.ts
+++ 1 projekt/src/index.ts
@@ -1,101 +1,137 @@
-class Project
-{
-    buttons: NodeListOf<HTMLInputElement> | undefined;
-    inputsData: number[] = [];
+class StatsApp{
+    
+    
+    Inputsnumber: HTMLInputElement; //Odpowiedzialny za ilosc generwowanych inputow
+    dataDiv;
+    Inputs ;
+   // dataInput1 :HTMLInputElement;
+    //dataInput2 :HTMLInputElement;
+    //dataInput3 :HTMLInputElement;
+   // dataInput4 :HTMLInputElement;
+    
+    
 
-    constructor()
-    {
-        this.init();
+    sumInput:HTMLInputElement;
+    avgInput:HTMLInputElement;
+    minInput:HTMLInputElement;
+    maxInput:HTMLInputElement;
+
+    //Konstruktor
+    constructor(){
+        this.startApp();
     }
 
-    init()
-    {
-        this.assignData();
-        this.assignEvents();
+    startApp(){
+        this.getInputs();
+        this.watchInputValues();
     }
 
-    assignData()
-    {
-        this.buttons = document.getElementById("inputs-container")?.querySelectorAll("input");
+    
+    //Query selectory do inputÃ³w
+    getInputs(){
+        this.Inputsnumber =document.querySelector('#inputs');
+        this.Inputs=document.querySelector('.input-data');
+        //this.dataInput1 =document.querySelector('#data1');
+        //this.dataInput2 =document.querySelector('#data2');
+        //this.dataInput3 =document.querySelector('#data3');
+        //this.dataInput4 =document.querySelector('#data4');
+        this.sumInput =document.querySelector('#sum');
+        this.avgInput =document.querySelector('#avg');
+        this.minInput =document.querySelector('#min');
+        this.maxInput =document.querySelector('#max');
+    }
+
+    //Tworzenie inputow
+    createInputs(){
+        const quantity= +this.Inputsnumber.value;
+        console.log(quantity);
+
+        this.clearContent(); //Jezeli cos instnialo wczesniej - usuwa to.
+
+        for(let i=1;i<=quantity;i++)
+        {
+            var input = document.createElement("input");
+            input.type = "text";
+            input.className = "data"+i;
+            input.oninput =function(){}
+
+            var button =document.createElement("button");
+            button.innerHTML = 'Delete';
+            button.className ="delete"+i;
+            button.onclick = function()
+            {
+                let inputDiv=document.querySelector('.input-data');
+                let classname =document.querySelector('.delete'+i);
+                let dataname =document.querySelector('.data'+i);
+                let brname =document.querySelector('.br'+i);
+                
+                inputDiv.removeChild(classname);
+                inputDiv.removeChild(dataname);
+                inputDiv.removeChild(brname);
+                
+            }
+
+            var br=document.createElement('br');
+            br.className ='br'+i;
+            
+            this.dataDiv =document.querySelector('.input-data').appendChild(input);
+            this.dataDiv =document.querySelector('.input-data').appendChild(button);
+            this.dataDiv =document.querySelector('.input-data').appendChild(br);
+            
+            
+        }
 
     }
 
-    assignEvents()
-    {
-        if(this.buttons != undefined) {
-            this.buttons.forEach((button: HTMLInputElement) => {
-                button.addEventListener("input", (e: any) => {
-                    this.updateInputsData();
-                    this.updateData();
-                });
-            });
+    clearContent(){
+        this.dataDiv = document.querySelector('.input-data');
+
+        while (this.dataDiv.firstChild) {
+            this.dataDiv.removeChild(this.dataDiv.firstChild);
         }
     }
 
-    updateInputsData()
-    {
-        let inputsData: number[] = [];
+    
 
-        if(this.buttons != undefined) {
-            this.buttons.forEach((button: HTMLInputElement) => {
-                if(this.isNumber(button.value))
-                    inputsData.push(Number(button.value));
-            });
-        }
+    watchInputValues() {
+        this.Inputsnumber.addEventListener('input', ()=>this.createInputs());
+        //this.dataInput1.addEventListener('input', () => this.computeData());
+        //this.dataInput2.addEventListener('input', () => this.computeData());
+        //this.dataInput3.addEventListener('input', () => this.computeData());
+        //this.dataInput4.addEventListener('input', () => this.computeData());
 
-        this.inputsData = inputsData;
+        
+        this.Inputs.addEventListener('keypress', ()=>this.computeData());
+        this.Inputs.addEventListener('click', ()=>this.computeData());
+        
+        //TO DO
+        //OGARNIJ ODSWIEZANIE
+        
+        
     }
 
-    countMin()
-    {
-        return Math.min.apply(Math, this.inputsData);
+    computeData(){
+        //const data1 = +this.dataInput1.value;
+       // const data2 = +this.dataInput2.value;
+        //const data3 = +this.dataInput3.value;
+        //const data4 = +this.dataInput4.value;
+
+        //const sum=data1+data2+data3+data4;
+        //const avg = sum/4;
+
+        //const min =Math.min(data1,data2,data3,data4);
+        //const max =Math.max(data1,data2,data3,data4);
+        //this.showStats(sum, avg, min, max);
+        console.log('dziala');
+
     }
 
-    countMax()
-    {
-        return Math.max.apply(Math, this.inputsData);
-    }
-
-    countAvg()
-    {
-        return this.countSum() / this.inputsData.length;
-    }
-
-    countSum()
-    {
-        let sum = 0;
-        this.inputsData.forEach(val => {
-            sum += val;
-        });
-
-        return sum;
-    }
-
-    updateData()
-    {
-        let inpSum: HTMLInputElement | null = document.querySelector("#sum");
-        let inpAvg: HTMLInputElement | null = document.querySelector("#avg");
-        let inpMin: HTMLInputElement | null = document.querySelector("#min");
-        let inpMax: HTMLInputElement | null = document.querySelector("#max");
-
-        if(inpSum != null) {
-            inpSum.value = String(this.countSum());
-        }
-        if(inpAvg != null) {
-            inpAvg.value = String(this.countAvg());
-        }
-        if(inpMin != null) {
-            inpMin.value = String(this.countMin());
-        }
-        if(inpMax != null) {
-            inpMax.value = String(this.countMax());
-        }
-    }
-
-    isNumber(value: string | number): boolean
-    {
-        return ((value != null) &&(value !== '') && !isNaN(Number(value.toString())));
+    showStats(sum:number,avg:number,min:number,max:number){
+        this.sumInput.value =sum.toString();
+        this.avgInput.value =avg.toString();
+        this.minInput.value =min.toString();
+        this.maxInput.value =max.toString();
     }
 }
 
-let project = new Project();
+const App =new StatsApp();
