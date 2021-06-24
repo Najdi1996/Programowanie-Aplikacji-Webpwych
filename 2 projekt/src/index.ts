07d2941af3ed4d99c32a9f68d269b4bdb0005572
Najdi1996 <perel99@onet.pl>
2021-06-24 22:05:47 +0200

    2 projekt cz.1

--- 2 projekt/src/index.ts
+++ 2 projekt/src/index.ts
@@ -1,129 +1,197 @@
-import Keyboard from "./Keyboard";
-import Recorder, { RecorderState } from "./Recorder";
-import DefaultKeyboardMapper from "./mappers/MappersKeyboard";
-import Player from "./Player";
-import { initLayers } from "./Layer";
+//Dzwiek
+let boomSound :HTMLAudioElement;
+let clapSound : HTMLAudioElement;
+let hihatSound :HTMLAudioElement;
+let kickSound: HTMLAudioElement;
+let operhantSound: HTMLAudioElement;
+let rideSound: HTMLAudioElement;
+let snareSound: HTMLAudioElement;
+let tinkSound: HTMLAudioElement;
+let tomSound: HTMLAudioElement;
 
-const numberOfLayers = 4;
-const recorders: Recorder[] = [];
-const players: Player[] = [];
-const keyboard = new Keyboard();
-keyboard.load();
-initLayers(numberOfLayers);
+//Play Channels
+let channel1:any[] =[];
+let channel2:any[] =[];
+let channel3:any[] =[];
+let channel4:any[] =[];
 
-for (let index = 0; index < numberOfLayers; index++) {
-    recorders[index] = new Recorder()
-    players[index] = new Player(recorders[index], keyboard);
+//Booleany odpowiedzialne za zezwolenie na nagrywanie kanalu
+let isChannel1Record:boolean =false;
+let isChannel2Record:boolean =false;
+let isChannel3Record:boolean =false;
+let isChannel4Record:boolean =false;
+
+let recordStartTime:any;
+
+//Wywolanie apki
+appStart();
+
+//Konstruktor
+function appStart() :void{
+    document.body.addEventListener('keypress', onKeyDown);
+    setChannels();
+    getSounds();
 }
 
+//Funkcja chwytajaca buttony channelow
+function setChannels():void{
+    document.querySelector('#chnl1').addEventListener('click', onPlayChannel1)
+    document.querySelector('#chnl2').addEventListener('click', onPlayChannel2)
+    document.querySelector('#chnl3').addEventListener('click', onPlayChannel3)
+    document.querySelector('#chnl4').addEventListener('click', onPlayChannel4)
 
+    document.querySelector('#rchnl1').addEventListener('click', onRecordChannel1)
+    document.querySelector('#rchnl2').addEventListener('click', onRecordChannel2)
+    document.querySelector('#rchnl3').addEventListener('click', onRecordChannel3)
+    document.querySelector('#rchnl4').addEventListener('click', onRecordChannel4)
 
-function onKeyPress(event: KeyboardEvent)
-{
-    let key = String(event.key).toLocaleLowerCase();
-    let time = event.timeStamp;
-
-    let mapper = DefaultKeyboardMapper.getMapper();
-    if(key in mapper) {
-        keyboard.setKey(mapper[key]);
-        keyboard.play();
-
-        for (let index = 0; index < numberOfLayers; index++) {
-            recorders[index]?.push(mapper[key], time);
-        }
-    }
+    document.querySelector('#stopr').addEventListener('click', onStopRecord)
 }
 
-function onClick(event: Event)
-{
-    let target = <HTMLElement> event.target;
-    let soundKey: (string | undefined) = target.dataset.sound;
-    let time = event.timeStamp;
+//Polaczenie dzwieku z HTML ze zmienna
+function getSounds() :void{
+    boomSound =document.querySelector('[data-sound="boom"]');
+    clapSound=document.querySelector('[data-sound="clap"]');
+    hihatSound=document.querySelector('[data-sound="hihat"]');
+    kickSound=document.querySelector('[data-sound="kick"]');
+    operhantSound=document.querySelector('[data-sound="operhant"]');
+    rideSound=document.querySelector('[data-sound="ride"]');
+    snareSound=document.querySelector('[data-sound="snare"]');
+    tinkSound=document.querySelector('[data-sound="tink"]');
+    tomSound=document.querySelector('[data-sound="tom"]');
     
-    if(soundKey != undefined) {
-        keyboard.setKey(soundKey);
-        keyboard.play();
-
-        for (let index = 0; index < numberOfLayers; index++) {
-            recorders[index]?.push(soundKey, time);
-        }
-    }
 }
 
-
-
-let recordBtns = document.querySelectorAll(".record-btn");
-let stopRecordingBtns = document.querySelectorAll(".stop-recording-btn");
-let playBtns = document.querySelectorAll(".play-btn");
-
-function getInputIndex(event: Event) {
-    const target = (event.target as HTMLElement);
-    return Number(target.dataset.key);
-}
-
-recordBtns?.forEach(btn => {
-    btn.addEventListener("click", function(event) {
-        const index = getInputIndex(event);
-        const recorder = recorders[index];
-
-        if(recorder.state == RecorderState.WAITING) {
-            console.log("RECORD BTN!");
-            recorder.clearChannel();
-            recorder.state = RecorderState.RECORDING;
-            recorder.startAt = event.timeStamp;
-
-            let recordBtn = document.querySelector(`button.record-btn[data-key="${index}"]`) as HTMLButtonElement;
-            recordBtn.disabled = true;
-
-            let stopRecordingBtn = document.querySelector(`button.stop-recording-btn[data-key="${index}"]`) as HTMLButtonElement;
-            stopRecordingBtn.disabled = false;
-        }
-    });
+//Odtworz Channel1
+function onPlayChannel1() :void{
+channel1.forEach(sound=>{
+    setTimeout(()=>playSound(sound.key),sound.time)
+    
 });
-
-stopRecordingBtns?.forEach(btn => {
-    btn.addEventListener("click", function(event) {
-        const index = getInputIndex(event);
-        const recorder = recorders[index];
+}
+//Odtworz Channel2
+function onPlayChannel2() :void{
+    channel2.forEach(sound=>{
+        setTimeout(()=>playSound(sound.key),sound.time)
         
-        if(recorder.state == RecorderState.RECORDING) {
-            console.log("STOP RECORDING BTN!");
-            recorder.state = RecorderState.WAITING;
-
-            let recordBtn = document.querySelector(`button.record-btn[data-key="${index}"]`) as HTMLButtonElement;
-            recordBtn.disabled = false;
-
-            let stopRecordingBtn = document.querySelector(`button.stop-recording-btn[data-key="${index}"]`) as HTMLButtonElement;
-            stopRecordingBtn.disabled = true;
-        }
     });
-});
-
-playBtns?.forEach(btn => {
-    btn.addEventListener("click", function(event) {
-        const index = getInputIndex(event);
-        const recorder = recorders[index];
-        const player = players[index];
-
-        if(recorder.state == RecorderState.WAITING) {
-            console.log("PLAY BTN!");
-            player.play();
-        }
+}
+//Odtworz Channel3
+function onPlayChannel3() :void{
+    channel3.forEach(sound=>{
+        setTimeout(()=>playSound(sound.key),sound.time)
+        
     });
-});
-
-
-
-document.addEventListener("keypress", onKeyPress);
-
-
-
-const keyboardPanel = document.getElementById("keyboardPanel");
-if(keyboardPanel != null) {
-    let keys = keyboardPanel.querySelectorAll(".keyboardKey");
-
-    keys?.forEach(key => {
-        key.addEventListener("click", onClick);
+}
+//Odtworz Channel4
+function onPlayChannel4() :void{
+    channel4.forEach(sound=>{
+        setTimeout(()=>playSound(sound.key),sound.time)
+        
     });
+}
+
+//Zezwól na nagranie kanału 1
+function onRecordChannel1(ev :Event):void{
+    channel1 =[]; // Jezeli cos bylo wczesniej nagrane zostanie to wyczyszczone
+    isChannel1Record =true;
+    isChannel2Record =false;
+    isChannel3Record =false;
+    isChannel4Record =false;
+    //Tylko jeden kanal moze byc nagrywany w danym czasie
+    recordStartTime=ev.timeStamp;
+}
+
+//Zezwól na nagranie kanału 2
+function onRecordChannel2(ev : Event):void{
+    channel2=[];
+    isChannel1Record =false;
+    isChannel2Record =true;
+    isChannel3Record =false;
+    isChannel4Record =false;
+    recordStartTime=ev.timeStamp;
+}
+
+//Zezwól na nagranie kanału 3
+function onRecordChannel3(ev:Event):void{
+    channel3=[];
+    isChannel1Record =false;
+    isChannel2Record =false;
+    isChannel3Record =true;
+    isChannel4Record =false;
+    recordStartTime=ev.timeStamp;
+}
+
+//Zezwól na nagranie kanału 4
+function onRecordChannel4(ev:Event):void{
+    channel4=[];
+    isChannel1Record =false;
+    isChannel2Record =false;
+    isChannel3Record =false;
+    isChannel4Record =true;
+    recordStartTime=ev.timeStamp;
+}
+
+//Zatrzymaj nagrywanie
+function onStopRecord():void{
+isChannel1Record =false;
+isChannel2Record=false;
+isChannel3Record =false;
+isChannel4Record =false;
+}
+
+//Nagraj wybrany kanał
+function RecordChannel(ev: KeyboardEvent):void {
+    const key=ev.key;
+    const stampTime =ev.timeStamp;
+    const time=stampTime-recordStartTime;
+
+if(isChannel1Record==true) {channel1.push({key,time})}//{key: key time:time}
+else if(isChannel2Record==true) {channel2.push({key,time})}
+else if(isChannel3Record==true) {channel3.push({key,time})}
+else if(isChannel4Record==true) {channel4.push({key,time})}
+}
+
+//Funkcja reagujaca na wcisniecie klawiatury
+function onKeyDown(ev: KeyboardEvent):void {
+const key=ev.key;
+console.log(ev);
+RecordChannel(ev);
+playSound(key);
+}
+
+
+//Odtworzenie dzwieku po kliknieciu
+function playSound(key:string):void{
+    switch(key){
+        case 'a':{hihatSound.currentTime =0;
+            hihatSound.play();
+        break;}
+        case 's':{boomSound.currentTime =0;
+            boomSound.play();
+        break;}
+        case 'd':{clapSound.currentTime =0;
+            clapSound.play();
+        break;}
+        case 'f':{kickSound.currentTime =0;
+            kickSound.play();
+        break;}
+        case 'g':{operhantSound.currentTime =0;
+            operhantSound.play();
+        break;}
+        case 'q':{rideSound.currentTime =0;
+            rideSound.play();
+        break;}
+        case 'w':{snareSound.currentTime =0;
+            snareSound.play();
+        break;}
+        case 'e':{tinkSound.currentTime =0;
+            tinkSound.play();
+        break;}
+        case 'r':{tomSound.currentTime =0;
+            tomSound.play();
+        break;}
+    }
+
 }
 
